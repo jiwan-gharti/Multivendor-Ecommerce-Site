0d680efdabc51bd2ca0021abf52d8e253c2b2025
Jiwan Gharti <ghartijiwan@gmail.com>
2021-05-16 13:23:42 +0545

    first commit

---
+++ NewAgeShopping/mainapp/static/js/mainpage.js
@@ -0,0 +1,38 @@
+
+
+document.getElementById("search").addEventListener("click",search);
+
+// console.log(l);
+// function search(){
+    
+// }
+
+function search(){
+    let search1 = document.getElementById('search1');
+    if (search1.style.display == "none"){
+        search1.style.display = "block";
+    }else{
+        search1.style.display = "none";
+    }
+}
+
+document.getElementById("review").addEventListener("click",review);
+document.getElementById("description").addEventListener("click",review);
+
+// console.log(l);
+// function search(){
+    
+// }
+
+function review(){
+    console.log('Clicked');
+    let review = document.getElementById('review-content');
+    let description = document.getElementById('description-content');
+    if (review.style.display == "none" & description.style.display == "block" ){
+        review.style.display = "block";
+        description.style.display = "none";
+    }else{
+        review.style.display = "none";
+        description.style.display = "block";
+    }
+}
